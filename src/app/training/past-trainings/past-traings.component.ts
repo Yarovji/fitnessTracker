7a430427908f0a6688ff18b3a81a73d8e8cdbb34
Yaroslav Virstiuk <yarovji@gmail.com>
2019-08-10 11:43:25 +0300

    1ST

---
+++ src/app/training/past-trainings/past-traings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-past-traings',
+  templateUrl: './past-traings.component.html',
+  styleUrls: ['./past-traings.component.css']
+})
+export class PastTrainingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
